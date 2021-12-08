b8ad6762a5bddb0119f732a09ce5c6a514134743
Jordyn <jordyn@example.com>
2021-12-07 20:56:47 -0500

    fixing some errors

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,5 +1,15 @@
-import ReactDOM from 'react-dom'
-import './App.css'
-import App from './App.js'
+import React from "react"
+import ReactDOM from "react-dom"
+import "./App.css"
+import  "index.css"
+import App from "./App.js"
+import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(<App />, document.querySelector('#root'))
+ReactDOM.render(
+    <React.StrictMode>
+    <App/>
+    </React.StrictMode>,
+    document.getElementById('root')
+);
+
+reportWebVitals();
